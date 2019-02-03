2e4733958fd8d53f427f81610ecebd15ef14d826
Saravanan Vijayakumaran <sarva.v@gmail.com>
2019-02-04 00:44:14 +0530

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+use rand::thread_rng;
+use digest::Digest;
+use sha2::Sha256;
+use secp256k1zkp as secp;
+use secp::Secp256k1;
+use secp::key::SecretKey;
+use secp::pedersen::Commitment;
+
+fn main() {
+    let mut hasher = Sha256::new();
+    hasher.input(b"Hello, world!");
+    let _result = hasher.result();
+    
+    let secp_inst = Secp256k1::with_caps(secp::ContextFlag::Commit);
+    let _commit = Commitment::from_vec(vec![0]);
+    let _sk = SecretKey::new(&secp_inst, &mut thread_rng());
+}
+
