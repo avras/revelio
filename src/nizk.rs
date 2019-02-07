40889b75e5cdd6f5bbba77fccd0ca18444371a18
Saravanan Vijayakumaran <sarva.v@gmail.com>
2019-02-07 12:55:40 +0530

    Began implementation of SPK of discrete logarithm

--- src/nizk.rs
+++ src/nizk.rs
@@ -1,4 +1,6 @@
+use rand::thread_rng;
 use secp256k1zkp as secp;
+use secp::Secp256k1;
 use secp::key::{SecretKey, PublicKey, ZERO_KEY};
 
 pub struct RevelioSPK {
@@ -19,4 +21,25 @@ impl RevelioSPK {
       s3: ZERO_KEY,
     }
   }
+
+  pub fn create_spk_from_decoykey (
+    output: PublicKey,
+    keyimage: PublicKey,
+    dkey: SecretKey,
+    blinding_point: PublicKey, // G
+    value_point: PublicKey,    // H
+    keyimage_point: PublicKey, // G'
+  ) -> RevelioSPK {
+    let mut rng = thread_rng();
+    let secp_inst = Secp256k1::with_caps(secp::ContextFlag::Commit);
+
+    let mut rspk = RevelioSPK::new();
+    let mut r3 = SecretKey::new(&secp_inst, &mut rng);
+    rspk.c1 = SecretKey::new(&secp_inst, &mut rng);
+    rspk.s1 = SecretKey::new(&secp_inst, &mut rng);
+    rspk.s2 = SecretKey::new(&secp_inst, &mut rng);
+
+    rspk
+  }
+
 }
