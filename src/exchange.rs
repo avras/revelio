e2d895848b8ccd2b5501de853f63388415dcec34
Saravanan Vijayakumaran <sarva.v@gmail.com>
2019-02-07 12:53:48 +0530

    Added basepoints and fixed a bug
    The commit call in create_keyimage had wrong order of base points
    Also exchange key and value order in create_keyimage

--- src/exchange.rs
+++ src/exchange.rs
@@ -2,7 +2,7 @@ use rand::{thread_rng, Rng};
 use rand::seq::SliceRandom;
 use secp256k1zkp as secp;
 use secp::Secp256k1;
-use secp::key::{SecretKey, PublicKey, ZERO_KEY};
+use secp::key::{SecretKey, PublicKey, ZERO_KEY, ONE_KEY};
 use secp::pedersen::Commitment;
 use secp::ffi;
 use secp::constants;
@@ -35,6 +35,9 @@ pub struct GrinExchange {
   own_keys: Vec<SecretKey>,
   own_amounts: Vec<u64>,
   decoy_keys: Vec<SecretKey>,
+  blinding_basepoint: PublicKey,
+  value_basepoint: PublicKey,
+  keyimage_basepoint: PublicKey,
 }
 
 impl GrinExchange{
@@ -65,7 +68,7 @@ impl GrinExchange{
         amounts[i] = rng.gen_range(0, MAX_AMOUNT_PER_OUTPUT);
         revproof.anon_list[i] = Secp256k1::commit(&secp_inst, amounts[i], okeys[i]).unwrap()
                                   .to_pubkey(&secp_inst).unwrap();
-        revproof.keyimage_list[i] = GrinExchange::create_keyimage(&okeys[i], amounts[i])
+        revproof.keyimage_list[i] = GrinExchange::create_keyimage(amounts[i], okeys[i])
                                       .to_pubkey(&secp_inst).unwrap();
       } else {
         let temp_sk = SecretKey::new(&secp_inst, &mut rng);
@@ -75,6 +78,13 @@ impl GrinExchange{
       }
     }
 
+    let bl_basepoint = Secp256k1::commit(&secp_inst, 0, ONE_KEY).unwrap()
+                          .to_pubkey(&secp_inst).unwrap();                 // 1*G + 0*H
+    let v_basepoint = Secp256k1::commit(&secp_inst, 1, ZERO_KEY).unwrap()
+                          .to_pubkey(&secp_inst).unwrap();                 // 0*G + 1*H
+    let ki_basepoint = GrinExchange::create_keyimage(0, ONE_KEY)
+                          .to_pubkey(&secp_inst).unwrap();                 // 1*G' +0*H
+
     GrinExchange {
       anon_list_size: alist_size,
       own_list_size: olist_size,
@@ -82,11 +92,14 @@ impl GrinExchange{
       own_keys: okeys,
       own_amounts: amounts,
       decoy_keys: dkeys,
+      blinding_basepoint: bl_basepoint,
+      value_basepoint: v_basepoint,
+      keyimage_basepoint: ki_basepoint,
     }
   }
 
   /// Generating key image commitment
-  fn create_keyimage(blinding: &SecretKey, amount: u64) -> Commitment {
+  fn create_keyimage(amount: u64, blinding: SecretKey) -> Commitment {
     let flag = ffi::SECP256K1_START_SIGN | ffi::SECP256K1_START_VERIFY;
     let ctx = unsafe { ffi::secp256k1_context_create(flag) };
 
@@ -98,8 +111,8 @@ impl GrinExchange{
         commit_i.as_mut_ptr(),
         blinding.as_ptr(),
         amount,
+        constants::GENERATOR_H.as_ptr(),
         constants::GENERATOR_PUB_J_RAW.as_ptr(),
-        constants::GENERATOR_G.as_ptr(),
       )
     };
 
