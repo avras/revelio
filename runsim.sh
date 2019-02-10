65a3b65e5cb0c765be9c10a3de863f409a77d2d5
Saravanan Vijayakumaran <sarva.v@gmail.com>
2019-02-10 14:16:26 +0530

    Added simulation bash script and num_iter=10 results

---
+++ runsim.sh
@@ -0,0 +1,12 @@
+cargo build --release
+cargo run --release 100 25 -n 10
+cargo run --release 100 50 -n 10
+cargo run --release 100 75 -n 10
+
+cargo run --release 1000 250 -n 10
+cargo run --release 1000 500 -n 10
+cargo run --release 1000 750 -n 10
+
+cargo run --release 10000 2500 -n 10
+cargo run --release 10000 5000 -n 10
+cargo run --release 10000 7500 -n 10
