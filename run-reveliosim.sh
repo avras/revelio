ae29effeb748fae04fac9c9d4ab339dbe70277bd
Saravanan Vijayakumaran <sarva.v@gmail.com>
2019-04-10 15:59:12 +0530

    Added fresh Revelio simulation results

--- run-reveliosim.sh
+++ run-reveliosim.sh
@@ -1,1 +1,12 @@
-cargo run --release 100 75 -n 1
+cargo build --release
+cargo run --release --bin revelio 100 25 -n 100
+cargo run --release --bin revelio 100 50 -n 100
+cargo run --release --bin revelio 100 75 -n 100
+
+cargo run --release --bin revelio 1000 250 -n 100
+cargo run --release --bin revelio 1000 500 -n 100
+cargo run --release --bin revelio 1000 750 -n 100
+
+cargo run --release --bin revelio 10000 2500 -n 100
+cargo run --release --bin revelio 10000 5000 -n 100
+cargo run --release --bin revelio 10000 7500 -n 100
